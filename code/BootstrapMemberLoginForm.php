e30e01be09fe078e8bb8e2d7014f278390035d01
Werner Krauß <werner.krauss@netwerkstatt.at>
2013-07-27 12:39:12 +0200

    icons for MemberLoginForm

--- code/BootstrapMemberLoginForm.php
+++ code/BootstrapMemberLoginForm.php
@@ -10,6 +10,39 @@ class BootstrapMemberLoginForm extends MemberLoginForm {
 		$this->Fields()->bootstrapify();
 		$this->Actions()->bootstrapify();
 		$this->setTemplate("BootstrapForm");
+                //wmk
+//		$this->setTemplate("BootstrapForm_Modal");
+                
+                $fields = $this->Fields();
+                
+                $mail = $fields->fieldByName('Email');
+                
+                if ($mail) {
+                    $mail->setIcon('user');
+                    $fields->replaceField('Email', $mail);
+                }
+                
+                $pass = $fields->fieldByName('Password');
+                
+                if ($pass) {
+                    $pass->setIcon('lock');
+                    $fields->replaceField('Password', $pass);
+                }               
+                
+                $actions = $this->Actions();
+                $loginBtn = $actions->fieldByName('action_dologin');
+                if ($loginBtn) {
+                    $loginBtn->setStyle('primary');
+                    $actions->replaceField('action_dologin', $loginBtn);
+                }
+                $logoutBtn = $actions->fieldByName('action_logout');
+                if ($logoutBtn) {
+                    $logoutBtn->setStyle('primary');
+                    $actions->replaceField('action_logout', $logoutBtn);
+                }
+                
+                $this->setFields($fields);
+                $this->setActions($actions);
 	}
 
 }
